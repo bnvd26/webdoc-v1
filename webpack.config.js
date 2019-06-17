417d40f205bcf2e28554bb03399a8b3bbb85e0df
benads <benjaminad5@yahoo.com>
2019-06-18 00:34:48 +0200

    init symfony & vueJS

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+
+    .setOutputPath('public/build/')
+
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+
+    .splitEntryChunks()
+
+    .enableSingleRuntimeChunk()
+    .enableVueLoader()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // enables @babel/preset-env polyfills
+    .configureBabel(() => {}, {
+        useBuiltIns: 'usage',
+        corejs: 3
+    })
+;
+
+module.exports = Encore.getWebpackConfig();
