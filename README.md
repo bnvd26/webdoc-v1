a726354ec0d092ebb08b1398c4e9f9acaf860ec7
benads <benjaminad5@yahoo.com>
2019-06-18 14:22:12 +0200

    begin API

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ webdoc using symfony &amp; vueJS
 
 `brew install composer`
 
-### Install yarn
+### To install the bundle 
 
-`brew install yarn`
+`yarn install`
 
 ### webpack
 
--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,9 +6,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HomeController  extends ApiController
+class ApiController  extends Controller
 {
-    /**
+      /**
      * @Route("/")
      */
     public function number()
@@ -22,4 +22,3 @@ class HomeController  extends ApiController
         ]);
     }
 }
-
