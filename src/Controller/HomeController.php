42ef7b4f6efc2dfbc78cab5cd9db09fd1908d6b9
retroscilo <globule.atgcu@gmail.com>
2019-06-20 11:15:42 +0200

    test

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController  extends Controller
+{
+         /**
+         * @Route("/")
+         */
+        public function slash()
+        {
+            return $this->render('pages/home.html.twig');
+        }
+
+
+
+
+        
+}
